7f7c9379915c3f9c2946786e3f077bf87c8e3734
aroselee <acroselee@gmail.com>
2022-04-06 21:48:49 -0700

    Touchup to middleware

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -2,12 +2,12 @@ import { NextRequest, NextResponse } from 'next/server'
 
 const signedinPages = ['/', '/playlist', '/library']
 
-export default function middleware(request: NextRequest) {
-  if (signedinPages.find((p) => p === request.nextUrl.pathname)) {
-    const token = request.cookies.TRAX_ACCESS_TOKEN
+export default function middleware(req: NextRequest) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN
 
     if (!token) {
-      const url = request.nextUrl.clone()
+      const url = req.nextUrl.clone()
       url.pathname = '/signin'
       return NextResponse.redirect(url)
     }
